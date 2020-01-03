cbf947ce87a53d4a673d2e85acdfce030c5ca311
Laura Daybell <laura.l.daybell@gmail.com>
2020-01-03 09:12:00 -0700

    updated resume

--- portfolio/src/component/Resume.js
+++ portfolio/src/component/Resume.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Resume = () => {
     return (
         <div className='resume'>
-            <a target="_blank" rel="noopener noreferrer" href="https://docs.google.com/document/d/1PnGHi53n-IpagUelENHapVNvIUXdgeQag_VfYrgh7L4/edit?usp=sharing"><button>Resum&eacute;</button></a>
+            <a target="_blank" rel="noopener noreferrer" href="https://drive.google.com/file/d/1azv5MDTZq0YNYZeE_A7rwSHNQF5Hc2tw/view?usp=sharing"><button>Resum&eacute;</button></a>
             
         </div>
     )
