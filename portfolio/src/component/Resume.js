126d72444a192440eead270e39e7573b65bfa48c
Laura Daybell <laura.l.daybell@gmail.com>
2020-09-27 19:09:22 -0600

    again

--- portfolio/src/component/Resume.js
+++ portfolio/src/component/Resume.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Resume = () => {
     return (
         <div className='resume'>
-            <a target="_blank" rel="noopener noreferrer" href="https://drive.google.com/file/d/1_n6BE9iGYOksNFfJHoymG9yQT-q5rp37/view?usp=sharing"><button>Resum&eacute;</button></a>
+            <a target="_blank" rel="noopener noreferrer" href="https://drive.google.com/file/d/11xPe7xHBiyA4oWJ5dy1MxTkUM6ZQwItF/view?usp=sharing"><button>Resum&eacute;</button></a>
             
         </div>
     )
